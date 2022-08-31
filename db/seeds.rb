ac772726ca5d13066d9ebdfbcb17ce8f8c8f4a70
jeremydimes <j10erten@gmail.com>
2022-08-30 18:38:08 -0600

    Kids's Toys

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(
+  name: "CarToy",
+  price: 10,
+  image_url: "toy",
+  description: "",
+)
+product.save
+
+product = Product.new(
+  name: "TrainToy",
+  price: 7,
+  image_url: "toy",
+  description: "",
+)
+product.save
+
+product = Product.new(
+  name: "
+  PlaneToy",
+  price: 4,
+  image_url: "",
+  description: "",
+)
+product.save
